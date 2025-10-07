4ccb15e5518681963049d44923ac6bbd0b30c041
joaohall <enderfiremite@gmail.com>
2025-10-07 04:01:43 -0300

    Fix no serviço de rotas no arquivo index

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from api.routes import router
 
 app = FastAPI(title="Microsoft Auth API", version="1.0.0")
 app.middleware("http")(auth_middleware)
-app.include_router(router)
+app.include_router(router, prefix="/api")
 
 
 @app.get("/api")
