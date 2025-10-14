38af605d7152bb56ea35ce30e73757a97bd46c48
joaohall <enderfiremite@gmail.com>
2025-10-14 04:52:38 -0300

    Alteração na variavel de ambiente do endpoint

--- src/app/screens/dashboard/Geral.tsx
+++ src/app/screens/dashboard/Geral.tsx
@@ -15,7 +15,7 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-const apiUrl = process.env.REACT_APP_API_ENDPOINT || 'https://sig-ct-api.vercel.app';
+const apiUrl = import.meta.env.VITE_API_ENDPOINT || 'https://sig-ct-api.vercel.app';
 
 interface Comunidade {
   id: number;
